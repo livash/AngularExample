ccd7d770cfb129e47b7e3db3049f221b6d4defe3
olena ivashyna <olena.ivashyna@gmail.com>
2016-06-03 20:49:24 -0600

    add xScale function

--- app/js/controllers/d3ExamplesController.js
+++ app/js/controllers/d3ExamplesController.js
@@ -17,4 +17,10 @@ squidApp.controller('d3ExamplesController',
         ];
         $scope.width = 600;
         $scope.height = 300;
+        $scope.margin = {
+          left: 10,
+          top: 0,
+          right: 10,
+          bottom: 0
+        }
 });
--- app/js/directives/d3Line.js
+++ app/js/directives/d3Line.js
@@ -3,10 +3,35 @@
 squidApp.directive('d3line', function() {
   
   // constants
-  var h = 100,
-      w = 400,
-      xScale = 3,
+  var height = 100,
+      width = 400,
       yScale = 2.5;
+      
+  var getMeasures = function(data) {
+    var xVals = [],
+        yVals = [];
+
+    data.forEach(function(el, i) {
+      xVals.push(el.x);
+      yVals.push(el.y);
+    });
+    xVals.sort();
+    yVals.sort();
+    
+    return {
+      xVals: xVals,
+      yVals: yVals
+    };
+  };
+  
+  var getMargin = function(margin) {
+      return {
+        left: !!(margin) ? margin.left : 0,
+        top: !!(margin) ? margin.top : 0,
+        right: !!(margin) ? margin.right : 0,
+        bottom: !!(margin) ? margin.bottom : 0
+      };
+  };
 
   return {
     restrict: 'E',
@@ -15,20 +40,29 @@ squidApp.directive('d3line', function() {
     scope: {
       data: '=',
       width: '=',
-      height: '='
+      height: '=',
+      margin: '='
     },
     link: function(scope, element, attrs) {
+      var measures = getMeasures(scope.data);
+      var margin = getMargin(scope.margin);
+      var w = scope.width ? scope.width : width;
+      var h = scope.height ? scope.height: height;
+      
+      var xScale = d3.scale.linear()
+                  .domain([measures.xVals[0], measures.xVals[measures.xVals.length - 1]])
+                  .range([0, w]);
       
       var lineFun = d3.svg.line()
-         .x(function(d) { return d.x * xScale; })
+         .x(function(d) { return xScale(d.x); })
          .y(function(d) { return h - d.y * yScale; })
          .interpolate("linear");
 
        var svg = d3.select(".d3-line")
                    .append("svg")
                    .attr({
-                     width: scope.width ? scope.width : w,
-                     height: scope.height ? scope.height: h
+                     width: w,
+                     height: h
                    })
                    .style("background-color", "pink");
 
@@ -47,7 +81,7 @@ squidApp.directive('d3line', function() {
          .append("text")
          .text(function(d) { return d.y; })
          .attr({
-           x: function(d) { return d.x * xScale;},
+           x: function(d) { return xScale(d.x);},
            y: function(d) {return h - d.y * yScale - 15;},
            "text-anchor": "middle",
            "font-size": "14px",
@@ -59,13 +93,14 @@ squidApp.directive('d3line', function() {
              }
            }
          });
-
+         
+        // add circles
        var circles = svg.selectAll("circle")
          .data(scope.data)
          .enter()
          .append("circle")
          .attr({
-           cx: function(d) {return d.x * xScale; },
+           cx: function(d) {return xScale(d.x); },
            cy: function(d) {return h - d.y * yScale;},
            r: 5,
            stroke: "#333",
