49f4e7c181c4d0e3eb9b51105a4cb2346c4677c3
Olena Deering <olena.ivashyna@gmail.com>
2016-06-04 11:56:11 -0600

    d3 column | add styling to tooltip and columns on hover | add title

--- app/js/directives/d3column.js
+++ app/js/directives/d3column.js
@@ -3,11 +3,11 @@
 squidApp.directive('d3column', function() {
   // defaults
   var width = 600,
-      height = 150,
+      height = 200,
       spacing = 10, // horizontal spacing between bars
       margin = {
         left: 25,
-        top: 25,
+        top: 50,
         right: 25,
         bottom: 25
       }
@@ -43,16 +43,38 @@ squidApp.directive('d3column', function() {
               .domain([sorted.yVals[0], sorted.yVals[sorted.yVals.length - 1]])
               .range([(height - margin.top - margin.bottom), 0]);
       
+      // create svg for the chart
       var svg = d3.select('.d3-chart')
-                .append("svg")
-                .attr({ width: width, height: height})
-                .attr('class', 'd3-column')
-                .style('background-color', '#5EFB6E');
+              .append("svg")
+              .attr({ width: width, height: height})
+              .attr('class', 'd3-column-chart')
+              .style('background-color', '#5EFB6E');
       
+      svg.append('text')
+          .attr('class', 'd3-column-chart-title')
+          .attr('x', margin.left)
+          .attr('y', margin.top - 25)
+          .text('Hover over columns');
+      
+      // all chart element are inside a group element
+      // it is used to apply margin to all elements of the chart
       var g = svg.append('g')
-                .attr('transform', "translate(" + margin.left + "," + margin.top + ")");
+              .attr('transform', "translate(" + margin.left + "," + margin.top + ")");
 
-      g.selectAll('rect')
+      // use external library for tootips in d3, called d3.tip
+      var tip = d3.tip()
+              .attr('class', 'd3-tip-column')
+              .attr('fill', '#ddd')
+              .offset([-10, 0])
+              .html(function(d, i) {
+                return "<strong>Value: </strong><span style='color: red;'>" + d.y + "</span>"; 
+              });
+     
+     // apply tooltip
+     g.call(tip);
+
+     // draw rectangles for columns/bars
+     var rect = g.selectAll('rect')
         .data(scope.data)
         .enter()
         .append('rect')
@@ -60,8 +82,9 @@ squidApp.directive('d3column', function() {
           .attr('y', function(d, i) { return yScale(d.y); })
           .attr('width', barWidth)
           .attr('height', function(d, i) { return height - yScale(d.y) - margin.bottom; })
-          .attr('fill', '#FA5CEA')
-          .attr('stroke', '#333');
+          .attr('class', 'd3-column')
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide); 
     }
   }
 });
