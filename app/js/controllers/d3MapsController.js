c0bf2827e1cac2893d9150326e4e2a974123f6fb
Olena Deering <olena.ivashyna@gmail.com>
2016-06-07 22:04:19 -0600

    UK Map | increase scale, adjust posiiton on the svg

--- app/js/controllers/d3MapsController.js
+++ app/js/controllers/d3MapsController.js
@@ -2,17 +2,27 @@ squidApp.controller('d3MapsController',
   function d3MapsController($scope) {
     $scope.title = "Map of the United Kingdom";
     
+    const width = 900,
+          height = 1000;
+    
     var svg = d3.select('.map-uk').append('svg')
-              .attr('width', 900)
-              .attr('height', 500)
+              .attr('width', width)
+              .attr('height', height)
               .style('background-color', '#fff');
     
     d3.json('./../../content/uk.json', (error, ukMap) => {
       if (error) return console.log(error);
       
+      var subunits = topojson.feature(ukMap, ukMap.objects.subunits);
+      var projection = d3.geo.mercator()
+          .scale(2500)
+          .translate([width - 300, height + 2400]);
+      var path = d3.geo.path()
+          .projection(projection);
+      
       svg.append('path')
-          .datum(topojson.feature(ukMap, ukMap.objects.subunits))
-          .attr('d', d3.geo.path().projection(d3.geo.mercator()));
+          .datum(subunits)
+          .attr('d', path);
     });
   
 });
