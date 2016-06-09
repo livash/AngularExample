0e595caacb3c4903412c7752ee849a5bde2b07ed
Olena Deering <olena.ivashyna@gmail.com>
2016-06-08 21:43:48 -0600

    d3 mapp | add colors to the map of UK and Ireland

--- app/js/controllers/d3MapsController.js
+++ app/js/controllers/d3MapsController.js
@@ -1,6 +1,6 @@
 squidApp.controller('d3MapsController',
   function d3MapsController($scope) {
-    $scope.title = "Map of the United Kingdom";
+    $scope.title = "Map of the United Kingdom and Ireland";
     
     const width = 900,
           height = 1000;
@@ -13,15 +13,18 @@ squidApp.controller('d3MapsController',
     d3.json('./../../content/uk.json', (error, ukMap) => {
       if (error) return console.log(error);
       
-      var subunits = topojson.feature(ukMap, ukMap.objects.subunits);
+      var subunits = topojson.feature(ukMap, ukMap.objects.subunits).features;
       var projection = d3.geo.mercator()
           .scale(2500)
           .translate([width - 300, height + 2400]);
       var path = d3.geo.path()
           .projection(projection);
       
-      svg.append('path')
-          .datum(subunits)
+      svg.selectAll('.subunit')
+          .data(subunits)
+          .enter()
+          .append('path')
+          .attr('class', function(d) { return "subunit " + d.id; })
           .attr('d', path);
     });
   
