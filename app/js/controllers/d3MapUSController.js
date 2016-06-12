32b13f71f265dfd7236961932035c70b53ea8f69
Olena Deering <olena.ivashyna@gmail.com>
2016-06-11 21:34:52 -0600

    d3 US map | add state color change on hover and tooltip

--- app/js/controllers/d3MapUSController.js
+++ app/js/controllers/d3MapUSController.js
@@ -1,6 +1,6 @@
 squidApp.controller('d3MapUSController', 
   function d3USMapController($scope) {
-    $scope.title = "Map of the United Statestop";
+    $scope.title = "Map of the United States";
     
     const width = 1000,
           height = 800;
@@ -14,7 +14,7 @@ squidApp.controller('d3MapUSController',
     d3.json('../../content/us-states-10m.json', function(error, usMap) {
       if (error) return console.log(error);
 
-      // select all counties from JSON
+      // select all states from JSON
       var states = topojson.feature(usMap, usMap.objects.states).features;
 
       // assign projection properties
@@ -26,7 +26,7 @@ squidApp.controller('d3MapUSController',
       var path = d3.geo.path()
           .projection(projection);
  
-      // draw counties
+      // draw states
       svg.selectAll('.state')
           .data(states)
           .enter()
@@ -34,7 +34,16 @@ squidApp.controller('d3MapUSController',
           .attr('class', d => `state ${d.id}`)
           .attr('d', path);
 
-      // draw county boundaries
+      // add tooltips showing the ID (and lateron name) of each state
+      var tip = d3.tip()
+          .attr('class', 'state-name-tip')
+          .attr('fill', '#ddd')
+          .offset([-10,0])
+          .html((d, i) => {console.log(d); return `State number ${d.id}`});
+
+      svg.call(tip);
+
+      // draw state boundaries
       svg.append('path')
         .datum(topojson.mesh(usMap, usMap.objects.states))
         .attr('d', path)
