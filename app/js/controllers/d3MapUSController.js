f25421b059f8597aef43a5b421b4774a1697504f
Olena Deering <olena.ivashyna@gmail.com>
2016-06-11 19:48:53 -0600

    d3 US map | switch from showing counties to showing states

--- app/js/controllers/d3MapUSController.js
+++ app/js/controllers/d3MapUSController.js
@@ -1,6 +1,6 @@
 squidApp.controller('d3MapUSController', 
   function d3USMapController($scope) {
-    $scope.title = "Map of the United States: counties";
+    $scope.title = "Map of the United Statestop";
     
     const width = 1000,
           height = 800;
@@ -15,7 +15,7 @@ squidApp.controller('d3MapUSController',
       if (error) return console.log(error);
 
       // select all counties from JSON
-      var counties = topojson.feature(usMap, usMap.objects.counties).features;
+      var states = topojson.feature(usMap, usMap.objects.states).features;
 
       // assign projection properties
       var projection = d3.geo.mercator()
@@ -27,17 +27,17 @@ squidApp.controller('d3MapUSController',
           .projection(projection);
  
       // draw counties
-      svg.selectAll('.county')
-          .data(counties)
+      svg.selectAll('.state')
+          .data(states)
           .enter()
           .append('path')
-          .attr('class', d => `county ${d.id}`)
+          .attr('class', d => `state ${d.id}`)
           .attr('d', path);
 
       // draw county boundaries
       svg.append('path')
-        .datum(topojson.mesh(usMap, usMap.objects.counties))
+        .datum(topojson.mesh(usMap, usMap.objects.states))
         .attr('d', path)
-        .attr('class', 'county-boundary');
+        .attr('class', 'state-boundary');
     });
 });
