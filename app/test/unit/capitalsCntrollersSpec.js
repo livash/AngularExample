ada24db25f6e9cd2e2968e2cd247d4043f0fad99
Olena Deering <olena.ivashyna@gmail.com>
2016-06-12 18:07:06 -0600

    capitalsController | unit tests

---
+++ app/test/unit/capitalsCntrollersSpec.js
@@ -0,0 +1,22 @@
+describe('capitalsController', function(){
+  var $controllerConstructor, scope
+
+  beforeEach(module('squidApp'));
+
+  beforeEach(inject(function($controller, $rootScope) {
+    $controllerConstructor = $controller;
+    scope = $rootScope.$new();
+  }))
+
+  it('should set the scope.countries to an array for four countries', function() {
+    
+    var ctrl = $controllerConstructor('capitalsController', {$scope: scope});
+    expect(scope.countries.length).toEqual(4);
+  });
+
+  it ('should set the scope.showCapitals to a function', function() {
+    var ctrl = $controllerConstructor('capitalsController', {$scope: scope});
+    
+    expect(typeof scope.showCapital).toBe('function');
+  })
+});
