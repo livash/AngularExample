5dcb856ad83308f3f0569432d72ee3f9328dedc4
Olena Deering <olena.ivashyna@gmail.com>
2016-08-07 10:15:16 -0600

    house cleaning

--- app/js/controllers/starWarsController.js
+++ app/js/controllers/starWarsController.js
@@ -2,7 +2,7 @@ squidApp.controller('starWarsController',
   function starWarsController(starWarsDataService) {
     let swCtrl = this;
     swCtrl.description = 'Data from STAR WARS API https://swapi.co/';
-    
+
     let success = (response) => swCtrl.people = response.data.results;
     let failure = (error) => swCtrl.status = 'Unable to retreive data';
     let getPeople = () => starWarsDataService.getAllPeople().then(success, failure);
